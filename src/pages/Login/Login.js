0d092b99ca1a9b00e755fcb1cb5c95ccb20b23c4
Magloire <magloire1@gmail.com>
2021-07-10 17:50:39 +0200

    removed logo

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,7 +6,7 @@ import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { ReactComponent as CalypsoLogo } from "../../calypso.svg";
+//import { ReactComponent as CalypsoLogo } from "../../calypso.svg";
 import logo from "../../calypso.svg";
 import axios from "axios";
 
