61f7f37fe22f99d4bd2745b57da4e7a731ff32c2
Kolopiolo <mb@watsonc.dk>
2022-10-06 15:13:06 +0200

    Added proxy and added changes to docker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AuthenticatedApp from "./AuthenticatedApp";
 import UnAuntenticatedApp from "./UnauthenticatedApp";
-
 
 function App() {
   const sessionId = sessionStorage.getItem("session_id");
   const [, setUser] = useState(sessionStorage.getItem("user") || null);
+
+  useEffect(() => {
+    fetch("/api", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
 
   if (sessionId === null) {
     return <UnAuntenticatedApp setUser={setUser} />;
