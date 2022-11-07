79854469607f86e25f110e4c721c6a01e4aa74e8
Kolopiolo <mb@watsonc.dk>
2022-11-07 12:03:11 +0100

    UD-10 Test commit

--- src/pages/admin/Kvalitetssikring/QualityAssurance.jsx
+++ src/pages/admin/Kvalitetssikring/QualityAssurance.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import BearingGraph from 'src/pages/field/Station/BearingGraph';
+import {useQuery} from '@tanstack/react-query';
+import {apiClient} from 'src/apiClient';
+
 const QualityAssurance = () => {
   let params = useParams();
+
+  const {data, isLoading, error} = useQuery(['test', params.ts_id], async ({signal}) => {
+    const {data} = await apiClient.get(`/sensor_admin/quick/${params.ts_id}`, {
+      signal,
+    });
+    return data;
+  });
 
   return <BearingGraph stationId={params.ts_id} measurements={[]} dynamicMeasurement={[]} />;
 };
