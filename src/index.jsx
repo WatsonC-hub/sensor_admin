83099830d28f2bebe2243d1c540b22e546c91a97
Kolopiolo <mb@watsonc.dk>
2022-11-18 10:55:27 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: 'https://bed215865bd94af0a00cf106f9e990b0@o4504178973474816.ingest.sentry.io/4504178982715392',
     integrations: [new BrowserTracing()],
-    environment: process.env.NODE_ENV,
+    environment: process.env.ENVIRONMENT,
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
