57a15b3326d799ced2bafbc64cbf81583153808b
Kolopiolo <mb@watsonc.dk>
2022-11-18 10:52:23 +0100

    Added sentry

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,20 @@ import {ToastContainer} from 'react-toastify';
 import {createRoot} from 'react-dom/client';
 import 'react-toastify/dist/ReactToastify.css';
 import theme from './theme';
+import * as Sentry from '@sentry/react';
+import {BrowserTracing} from '@sentry/tracing';
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://bed215865bd94af0a00cf106f9e990b0@o4504178973474816.ingest.sentry.io/4504178982715392',
+    integrations: [new BrowserTracing()],
+    environment: process.env.NODE_ENV,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 const queryClient = new QueryClient();
 const container = document.getElementById('root');
