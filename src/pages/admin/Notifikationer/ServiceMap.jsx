ac4faf62800a27ccbebe2762ff953a81b9347578
Kolopiolo <mb@watsonc.dk>
2023-03-23 10:05:50 +0100

    Update ServiceMap.jsx

--- src/pages/admin/Notifikationer/ServiceMap.jsx
+++ src/pages/admin/Notifikationer/ServiceMap.jsx
@@ -95,13 +95,13 @@ function Map({data, isLoading, setLassoFilter}) {
     var map = L.map('map', {
       center: [55.876823, 8.961644],
       zoom: 7,
-      layers: [outdormapbox],
+      layers: [toposkaermkortwmts],
       tap: false,
     });
 
     var baseMaps = {
-      OpenStreetMap: outdormapbox,
       Vandløb: toposkaermkortwmts,
+      OpenStreetMap: outdormapbox,
       Satelit: satelitemapbox,
     };
 
