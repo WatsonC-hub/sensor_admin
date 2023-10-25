ea35f57a64cc0c168e75bb432db5b873fe3b0063
MathiasBuus <mb@watsonc.dk>
2023-10-25 13:31:44 +0200

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,8 @@
 import {CacheableResponsePlugin} from 'workbox-cacheable-response';
 import {clientsClaim} from 'workbox-core';
 import {ExpirationPlugin} from 'workbox-expiration';
-import {cleanupOutdatedCaches, precacheAndRoute} from 'workbox-precaching';
-import {NavigationRoute, createHandlerBoundToURL, registerRoute} from 'workbox-routing';
+import {cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute} from 'workbox-precaching';
+import {NavigationRoute, registerRoute} from 'workbox-routing';
 import {CacheFirst, NetworkFirst} from 'workbox-strategies';
 
 self.skipWaiting();
