2cc41cff0a4cf8217ed33a6aa13da6c1d8a128be
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2024-06-04 13:01:07 +0200

    test

--- src/pages/field/station/ErrorPage.jsx
+++ src/pages/field/station/ErrorPage.jsx
@@ -1,5 +1,3 @@
-// Component to show when there is an error in the station page
-
 import {Box, Link, Typography} from '@mui/material';
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
