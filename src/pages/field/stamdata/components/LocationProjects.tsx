8fa11444c208cb32f99b0e10b5b73049db2bf8c2
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2024-08-14 16:00:15 +0200

    WAT-363

--- src/pages/field/stamdata/components/LocationProjects.tsx
+++ src/pages/field/stamdata/components/LocationProjects.tsx
@@ -80,8 +80,8 @@ const LocationProjects = ({value, setValue, error, disable}: LocationProjectsPro
             variant="outlined"
             error={Boolean(error) && superUser}
             helperText={Boolean(error) && superUser && error?.message}
-            label="Projekter"
-            placeholder="Indtast projekt nummer..."
+            label="Projekt nummber"
+            placeholder="Vælg projekt nummer..."
             sx={{
               pb: 0,
               '& .MuiInputBase-input.Mui-disabled': {
