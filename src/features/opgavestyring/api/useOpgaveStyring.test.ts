e88de64155ac00af7555f586032ee556174ae3d5
MathiasBuus <88654509+MathiasBuus@users.noreply.github.com>
2024-11-27 14:12:48 +0100

    Update useOpgaveStyring.test.ts

--- src/features/opgavestyring/api/useOpgaveStyring.test.ts
+++ src/features/opgavestyring/api/useOpgaveStyring.test.ts
@@ -1,5 +1,7 @@
 import {test, expect} from 'vitest';
 
+import('~/features/opgavestyring/api/useOpgaveStyring');
+
 test('Husk at teste context menu da den kan være påvirket', async () => {
   expect(true).toBe(true);
 });
