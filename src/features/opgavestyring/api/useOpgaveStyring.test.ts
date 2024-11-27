495a5b2d1a31c4573d042f0a5041512fdd491782
MathiasBuus <88654509+MathiasBuus@users.noreply.github.com>
2024-11-27 14:22:23 +0100

    Revert "Update useOpgaveStyring.test.ts"
    This reverts commit e88de64155ac00af7555f586032ee556174ae3d5.

--- src/features/opgavestyring/api/useOpgaveStyring.test.ts
+++ src/features/opgavestyring/api/useOpgaveStyring.test.ts
@@ -1,7 +1,5 @@
 import {test, expect} from 'vitest';
 
-import('~/features/opgavestyring/api/useOpgaveStyring');
-
 test('Husk at teste context menu da den kan være påvirket', async () => {
   expect(true).toBe(true);
 });
