c66f676c7c677d3ee0fa9e823e1a1804046dfca3
MathiasBuus <88654509+MathiasBuus@users.noreply.github.com>
2024-11-28 11:46:40 +0100

    New test

--- src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import {render} from '@testing-library/react';
-import {describe, it, expect} from 'vitest';
-
-describe('App', () => {
-  it('should render', () => {
-    expect(true).toBe(true);
-  });
-});
---
+++ src/helpers/dateConverter.test.ts
@@ -0,0 +1,10 @@
+import {describe, it, expect} from 'vitest';
+
+import {convertDate} from './dateConverter';
+
+describe('dateConverter', () => {
+  it('should convert date to string', () => {
+    const date = new Date('2021-01-01T00:00:00Z').toISOString();
+    expect(convertDate(date)).toBe('2021-01-01');
+  });
+});
