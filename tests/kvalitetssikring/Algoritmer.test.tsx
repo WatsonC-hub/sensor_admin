c9dd9c0f9c143682691af8f6cf158f5308de3f71
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2024-12-02 12:43:21 +0100

    mere test cases

---
+++ tests/kvalitetssikring/Algoritmer.test.tsx
@@ -0,0 +1,14 @@
+import {describe, it, expect} from 'vitest';
+
+import '~/pages/admin/kvalitetssikring/Algorithms';
+import '~/pages/admin/kvalitetssikring/AlgorithmCard';
+import '~/features/kvalitetssikring/api/useAlgorithms';
+
+describe('Test Algoritmer', () => {
+  it('Check at deaktiver algoritmer virker', () => {
+    expect(true).toBe(true);
+  });
+  it('Check at de rigtige algoritmer bliver vist', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ tests/kvalitetssikring/QaGraph.test.tsx
@@ -0,0 +1,21 @@
+import {describe, it, expect} from 'vitest';
+
+import '~/pages/admin/kvalitetssikring/QAGraph';
+import '~/features/kvalitetssikring/components/usePlotlyLayout';
+import '~/components/PlotlyGraph';
+import '~/pages/admin/kvalitetssikring/components/DataToShow';
+
+describe('Test QA graf', () => {
+  it('Check at zoom niveauer virker korrekt', () => {
+    expect(true).toBe(true);
+  });
+  it('Check at QA labels virker korrekt', () => {
+    expect(true).toBe(true);
+  });
+  it('Check at genberegn data virker korrekt', () => {
+    expect(true).toBe(true);
+  });
+  it('Check at genberegn QA virker korrekt', () => {
+    expect(true).toBe(true);
+  });
+});
