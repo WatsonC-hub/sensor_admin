1ce66e80b307e01fa80b24f5741ccfc622b35808
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2025-05-06 08:48:03 +0200

    Fixed fallback and added scanner

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,6 +6,7 @@ import {RemoveTrailingSlash} from '~/RemoveTrailingSlash';
 import TasksPage from './pages/admin/opgaver/TasksPage';
 import OpretStamdata from './pages/field/stamdata/OpretStamdata';
 import {AppContext} from './state/contexts';
+import ScanComponent from './components/ScanComponent';
 
 const Home = () => {
   return (
@@ -28,6 +29,15 @@ const Home = () => {
             </AppContext.Provider>
           }
         />
+        <Route
+          path="*"
+          element={
+            <AppContext.Provider value={{}}>
+              <TasksPage />
+            </AppContext.Provider>
+          }
+        />
+        <Route path="/:labelid" element={<ScanComponent />} />
       </Routes>
     </>
   );
