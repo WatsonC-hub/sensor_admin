c3712ba2acc8ec72d62032cc5d10be24d8e56023
MathiasBuus <88654509+MathiasBuus@users.noreply.github.com>
2025-05-13 10:38:31 +0200

    Fixed logic in all pejling forms

--- src/features/station/components/pejling/components/tables/PejlingBoreholeTableMobile.tsx
+++ src/features/station/components/pejling/components/tables/PejlingBoreholeTableMobile.tsx
@@ -15,10 +15,10 @@ import {MergeType, TableTypes} from '~/helpers/EnumHelper';
 import RenderActions from '~/helpers/RowActions';
 import {useTimeseriesData} from '~/hooks/query/useMetadata';
 import {useQueryTable} from '~/hooks/useTable';
-import {BoreholePejlingItem} from '~/types';
+import {PejlingItem} from '~/types';
 
 interface Props {
-  handleEdit: (kontrol: BoreholePejlingItem) => void;
+  handleEdit: (kontrol: PejlingItem) => void;
   handleDelete: (gid: number) => void;
   disabled: boolean;
 }
@@ -39,7 +39,7 @@ export default function PejlingBoreholeTableMobile({handleEdit, handleDelete, di
 
   const {get} = usePejling();
 
-  const columns = useMemo<MRT_ColumnDef<BoreholePejlingItem>[]>(
+  const columns = useMemo<MRT_ColumnDef<PejlingItem>[]>(
     () => [
       {
         accessorFn: (row) => row,
@@ -88,7 +88,7 @@ export default function PejlingBoreholeTableMobile({handleEdit, handleDelete, di
     [unit, disabled, handleEdit]
   );
 
-  const options: Partial<MRT_TableOptions<BoreholePejlingItem>> = {
+  const options: Partial<MRT_TableOptions<PejlingItem>> = {
     localization: {noRecordsToDisplay: 'Ingen kontrolmålinger at vise'},
     renderDetailPanel: ({row}) => (
       <Box sx={renderDetailStyle}>
@@ -126,7 +126,7 @@ export default function PejlingBoreholeTableMobile({handleEdit, handleDelete, di
     ),
   };
 
-  const table = useQueryTable<BoreholePejlingItem>(
+  const table = useQueryTable<PejlingItem>(
     columns,
     get,
     options,
