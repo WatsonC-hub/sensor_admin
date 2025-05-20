1848c937c18043895e0551d462cd771c59a24e86
MathiasBuus <88654509+MathiasBuus@users.noreply.github.com>
2025-05-20 14:13:28 +0200

    Updated grid view of Images

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -1,4 +1,4 @@
-import {Grid} from '@mui/material';
+import {Grid2} from '@mui/material';
 import React from 'react';
 
 import ImageCard from '~/components/ImageCard';
@@ -14,25 +14,23 @@ type ImageViewerProps = {
 function ImageViewer({images, deleteMutation, handleEdit}: ImageViewerProps) {
   const {isTouch} = useBreakpoints();
   return (
-    <>
+    <Grid2 container spacing={2} mx="auto">
       {images?.map((elem, index) => {
         return (
-          <Grid
-            item
-            mobile={12}
-            tablet={12}
-            laptop={6}
-            desktop={6}
-            xl={6}
+          <Grid2
+            size={{
+              mobile: 12,
+              laptop: 6,
+            }}
             key={elem.gid}
             display={'flex'}
             justifyContent={isTouch ? 'center' : index % 2 === 0 ? 'end' : 'start'}
           >
             <ImageCard image={elem} deleteMutation={deleteMutation} handleEdit={handleEdit} />
-          </Grid>
+          </Grid2>
         );
       })}
-    </>
+    </Grid2>
   );
 }
 
