2b7aa1d870753105074a85d8004db2e70501568d
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2025-06-06 15:28:22 +0200

    WIP

--- src/pages/field/station/alarms/Alarms.tsx
+++ src/pages/field/station/alarms/Alarms.tsx
@@ -12,7 +12,7 @@ import {useAppContext} from '~/state/contexts';
 import MoreTimeIcon from '@mui/icons-material/MoreTime';
 import {useShowFormState, useStationPages} from '~/hooks/useQueryStateParameters';
 import {apiClient} from '~/apiClient';
-import {Alarm, alarmTable} from '~/types';
+import {Alarm, AlarmContact, alarmTable} from '~/types';
 const alarmsSchema = z.object({
   name: z.string().min(1, 'Name is required'),
   attentionLevel: z.enum(['upperAttention', 'lowerAttention', 'upperAlarm', 'lowerAlarm'], {
@@ -22,14 +22,27 @@ const alarmsSchema = z.object({
   interval: z.number().min(1, 'Interval must be at least 1 hour'),
 });
 
+const alarmContactSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  sms: z.boolean(),
+  email: z.boolean(),
+  call: z.boolean(),
+  alarm_interval: z.number().min(1, 'Alarm interval must be at least 1 hour'),
+});
+
+export type AlarmContactFormValues = z.infer<typeof alarmContactSchema>;
+const AlarmContactForm = createTypedForm<AlarmContactFormValues>();
+
 export type AlarmsFormValues = z.infer<typeof alarmsSchema>;
-const Form = createTypedForm<AlarmsFormValues>();
+const AlarmForm = createTypedForm<AlarmsFormValues>();
 
 const Alarms = () => {
   const [pageToShow] = useStationPages();
   const [showForm] = useShowFormState();
   const [open, setOpen] = React.useState(false);
   const {ts_id} = useAppContext(['ts_id']);
+  const [tempAlarms, setTempAlarms] = React.useState<Array<Alarm>>([]);
+  const [tempAlarmContacts, setTempAlarmContacts] = React.useState<Array<AlarmContact>>([]);
 
   const alarmMethods = useForm<AlarmsFormValues>({
     resolver: zodResolver(alarmsSchema),
@@ -38,6 +51,17 @@ const Alarms = () => {
       attentionLevel: 'upperAttention',
       criteria: '',
       interval: 1,
+    },
+  });
+
+  const alarmContactMethods = useForm<AlarmContactFormValues>({
+    resolver: zodResolver(alarmContactSchema),
+    defaultValues: {
+      name: '',
+      sms: false,
+      email: false,
+      call: false,
+      alarm_interval: 1,
     },
   });
 
@@ -68,6 +92,13 @@ const Alarms = () => {
     // Here you would typically send the data to your API or handle it as needed
   };
 
+  const pushAlarm = (data: AlarmsFormValues) => {
+    setTempAlarms((prev) => [
+      ...prev,
+      {name: data.name, criteria: data.criteria, alarm_interval: data.interval},
+    ]);
+  };
+
   const cancel = () => {
     setOpen(false);
     alarmMethods.reset();
@@ -78,30 +109,64 @@ const Alarms = () => {
       <Dialog open={open} onClose={cancel} fullWidth maxWidth="sm">
         <DialogTitle>Alarmer</DialogTitle>
         <DialogContent>
-          <Form formMethods={alarmMethods} label="Alarm">
-            <Form.Input name="name" label="Alarm Name" placeholder="Enter alarm name" />
-            <Form.Input select name="attentionLevel" label="Attention Level">
+          <AlarmForm formMethods={alarmMethods} label="Alarm">
+            <AlarmForm.Input name="name" label="Alarm Name" placeholder="Enter alarm name" />
+            <AlarmForm.Input select name="attentionLevel" label="Attention Level">
               <MenuItem value="upperAttention">Upper Attention</MenuItem>
               <MenuItem value="lowerAttention">Lower Attention</MenuItem>
               <MenuItem value="upperAlarm">Upper Alarm</MenuItem>
               <MenuItem value="lowerAlarm">Lower Alarm</MenuItem>
-            </Form.Input>
-            <Form.Input
+            </AlarmForm.Input>
+            <AlarmForm.Input
               name="criteria"
               label="Criteria"
               placeholder="Enter criteria for the alarm"
             />
-            <Form.Input
+            <AlarmForm.Input
               name="interval"
               label="Interval (hours)"
               type="number"
               placeholder="Enter interval in hours"
             />
             <Box ml={'auto'} display="flex" gap={1}>
-              <Form.Cancel cancel={cancel} />
-              <Form.Submit submit={handleSubmit} />
+              <AlarmForm.Cancel cancel={cancel} />
+              <AlarmForm.Submit submit={pushAlarm} />
             </Box>
-          </Form>
+          </AlarmForm>
+          <AlarmContactForm formMethods={alarmContactMethods} label="Alarm Contact">
+            {/* Lav laves om til et select med alarm kontakter som options */}
+            <AlarmContactForm.Input
+              name="name"
+              label="Contact Name"
+              placeholder="Enter contact name"
+            />
+            <AlarmContactForm.Input
+              name="alarm_interval"
+              label="Alarm Interval (hours)"
+              type="number"
+              placeholder="Enter interval in hours"
+            />
+            <AlarmContactForm.Checkbox name="sms" label="SMS" gridSizes={{xs: 12, sm: 2}} />
+            <AlarmContactForm.Checkbox name="email" label="Email" gridSizes={{xs: 12, sm: 2}} />
+            <AlarmContactForm.Checkbox name="call" label="Call" gridSizes={{xs: 12, sm: 2}} />
+            <Box ml={'auto'} display="flex" gap={1}>
+              <AlarmContactForm.Cancel cancel={cancel} />
+              <AlarmContactForm.Submit
+                submit={(data) => {
+                  setTempAlarmContacts((prev) => [
+                    ...prev,
+                    {
+                      name: data.name,
+                      sms: data.sms,
+                      email: data.email,
+                      call: data.call,
+                      alarm_interval: data.alarm_interval,
+                    },
+                  ]);
+                }}
+              />
+            </Box>
+          </AlarmContactForm>
         </DialogContent>
       </Dialog>
       <AlarmTable alarm={alarm} />
