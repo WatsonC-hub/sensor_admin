ee831ac2af6881b42a291f1f62259fa39cdd1cc1
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2025-07-01 10:54:05 +0200

    Update useLocationForm.ts

--- src/features/station/api/useLocationForm.ts
+++ src/features/station/api/useLocationForm.ts
@@ -63,12 +63,6 @@ const getSchemaAndForm = <T extends FieldValues>(
       break;
   }
 
-  if (loctype_id === 9 && mode === 'Add' && loc_id !== undefined) {
-    selectedSchema = selectedSchema.extend({
-      boreholeno: z.string().nullish(),
-    });
-  }
-
   if (superUser === false || (mode === 'Add' && loc_id !== undefined)) {
     selectedSchema = selectedSchema.extend({
       initial_project_no: z.string().nullish(),
