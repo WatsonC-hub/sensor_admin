9321278631cbd38fcfce1cf84214ccf29544f7b2
MathiasBuus <88654509+MathiasBuus@users.noreply.github.com>
2025-08-28 11:23:43 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -51,7 +51,7 @@ const Button = ({bttype, children, ...props}: MyButtonProps) => {
     };
   }
 
-  sx = merge(sx, props.sx);
+  sx = merge({}, sx, props.sx);
 
   return (
     <MuiButton {...props} variant={bttype == 'link' ? 'text' : 'outlined'} sx={sx}>
