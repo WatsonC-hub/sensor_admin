667f641df3cd61ee3575c3afd399c3e5f99ecc67
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2026-01-13 13:49:46 +0100

    Update store.ts

--- src/features/map/store.ts
+++ src/features/map/store.ts
@@ -27,7 +27,7 @@ const mapFilterStore = create<MapFilterState>()(
     {
       name: 'calypso-map-filter',
       storage: createJSONStorage(() => localStorage),
-      version: 2,
+      version: 3,
       merge: (persistedState, currentState) => {
         const merged = merge({filters: {...defaultMapFilter()}}, currentState, persistedState);
         return merged;
