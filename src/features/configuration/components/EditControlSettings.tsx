9e5e659c134073aa97e1b849e6556f80ea9ff5b9
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2026-06-04 16:16:59 +0200

    Update EditControlSettings.tsx

--- src/features/configuration/components/EditControlSettings.tsx
+++ src/features/configuration/components/EditControlSettings.tsx
@@ -1,18 +1,20 @@
 import {Grid2} from '@mui/material';
 import React from 'react';
 import ControlSettings from './ControlSettings';
+import useBreakpoints from '~/hooks/useBreakpoints';
 
 type EditControlSettingsProps = {
   disabled: boolean;
 };
 
 const EditControlSettings = ({disabled}: EditControlSettingsProps) => {
+  const {isMobile} = useBreakpoints();
   return (
-    <Grid2 container spacing={2}>
-      <Grid2 size={6}>
+    <Grid2 container size={12} spacing={2}>
+      <Grid2 size={isMobile ? 12 : 6}>
         <ControlSettings.ControlFrequency disabled={disabled} />
       </Grid2>
-      <Grid2 size={6}>
+      <Grid2 size={isMobile ? 12 : 6}>
         <ControlSettings.LeadTime disabled={disabled} />
       </Grid2>
     </Grid2>
