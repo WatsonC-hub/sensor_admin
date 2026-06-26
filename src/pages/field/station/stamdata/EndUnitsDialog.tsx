458d8a21a392e1392e1c5c28855bb1c6ca62ff6a
Elyas Bahodi <43791649+elyasbahodi@users.noreply.github.com>
2026-06-26 15:44:56 +0200

    removed empty fragment

--- src/pages/field/station/stamdata/EndUnitsDialog.tsx
+++ src/pages/field/station/stamdata/EndUnitsDialog.tsx
@@ -146,133 +146,128 @@ const EndUnitsDialog = ({open, onClose}: UnitDialogProps) => {
   };
 
   return (
-    <>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Tilføj tidsserier på baggrund af udstyr</DialogTitle>
-        <DialogContent>
-          <FormProvider {...formMethods}>
-            <StamdataUnit tstype_id={tstype_ids}>
-              <Grid2 container>
-                <StamdataUnit.EndDate required />
-              </Grid2>
-            </StamdataUnit>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Tilføj tidsserier på baggrund af udstyr</DialogTitle>
+      <DialogContent>
+        <FormProvider {...formMethods}>
+          <StamdataUnit tstype_id={tstype_ids}>
+            <Grid2 container>
+              <StamdataUnit.EndDate required />
+            </Grid2>
+          </StamdataUnit>
 
-            {superUser && (
-              <Box display="flex" flexDirection="column" gap={1}>
-                <FormInput
-                  name="change_reason"
-                  fullWidth
-                  select
-                  label="Årsag"
-                  options={changeReasons?.map((reason) => ({[reason.id]: reason.reason}))}
-                  keyType="number"
-                  placeholder="Vælg årsag"
-                  onChangeCallback={(e) => {
-                    const reason = changeReasons?.find(
-                      (reason) =>
-                        reason.id ===
-                        Number((e as React.ChangeEvent<HTMLInputElement>).target.value)
-                    );
-                    if (reason) {
-                      if (reason.default_actions?.includes('CLOSE')) {
-                        const action = actions?.find((action) => action.action.includes('CLOSE'));
-                        if (action) {
-                          setValue('action', action.action);
-                        }
-                      } else {
-                        setValue('action', reason.default_actions ?? 'DO_NOTHING');
+          {superUser && (
+            <Box display="flex" flexDirection="column" gap={1}>
+              <FormInput
+                name="change_reason"
+                fullWidth
+                select
+                label="Årsag"
+                options={changeReasons?.map((reason) => ({[reason.id]: reason.reason}))}
+                keyType="number"
+                placeholder="Vælg årsag"
+                onChangeCallback={(e) => {
+                  const reason = changeReasons?.find(
+                    (reason) =>
+                      reason.id === Number((e as React.ChangeEvent<HTMLInputElement>).target.value)
+                  );
+                  if (reason) {
+                    if (reason.default_actions?.includes('CLOSE')) {
+                      const action = actions?.find((action) => action.action.includes('CLOSE'));
+                      if (action) {
+                        setValue('action', action.action);
                       }
+                    } else {
+                      setValue('action', reason.default_actions ?? 'DO_NOTHING');
                     }
-                  }}
-                />
+                  }
+                }}
+              />
 
-                <FormInput
-                  name="action"
-                  fullWidth
-                  select
-                  label="Handling"
-                  placeholder="Handling"
-                  options={actions?.map((action) => ({[action.action]: action.label}))}
-                />
+              <FormInput
+                name="action"
+                fullWidth
+                select
+                label="Handling"
+                placeholder="Handling"
+                options={actions?.map((action) => ({[action.action]: action.label}))}
+              />
 
-                <FormInput
-                  name="comment"
-                  label="Kommentar"
-                  fullWidth
-                  multiline
-                  rows={4}
-                  placeholder="Skriv en kommentar"
-                />
-              </Box>
-            )}
-          </FormProvider>
-          <Box>
-            {checkedSensors && checkedSensors.length > 0 && (
-              <List>
-                <Typography variant="subtitle1" sx={{mt: 2}}>
-                  Tilgængelige tidsserier:
-                </Typography>
-                {unit_history?.map((sensor) => (
-                  <Box
-                    key={sensor.uuid}
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                  >
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          checked={checkedSensors.some((s) => s.uuid === sensor.uuid)}
-                          onChange={(e) => {
-                            if (e.target.checked) {
-                              setCheckedSensors((prev) => [...prev, sensor]);
-                            } else {
-                              setCheckedSensors((prev) =>
-                                prev.filter((s) => s.uuid !== sensor.uuid)
-                              );
-                            }
-                          }}
-                        />
-                      }
-                      label={
-                        <Typography>
-                          {sensor.signal_id} - {sensor.sensor_id} (
-                          {activeTimeseries?.find((ts) => ts.ts_id === sensor.ts_id)?.ts_name})
-                        </Typography>
-                      }
-                    />
-                  </Box>
-                ))}
-              </List>
-            )}
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button bttype="tertiary" onClick={handleClose}>
-            Annuller
-          </Button>
-          <Button
-            bttype="primary"
-            disabled={checkedSensors.length === 0 || Object.keys(errors).length > 0}
-            onClick={handleSubmit(async (data) => {
-              await takeHomeMutation({
-                enddate: data.enddate,
-                change_reason: data.change_reason,
-                action: data.action,
-                comment: data.comment,
-                units: checkedSensors.map((sensor) => ({
-                  ts_id: sensor.ts_id,
-                  gid: sensor.gid,
-                  startdate: dayjs(sensor.startdato),
-                })),
-              });
-            })}
-          >
-            Hjemtag udstyr
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+              <FormInput
+                name="comment"
+                label="Kommentar"
+                fullWidth
+                multiline
+                rows={4}
+                placeholder="Skriv en kommentar"
+              />
+            </Box>
+          )}
+        </FormProvider>
+        <Box>
+          {checkedSensors && checkedSensors.length > 0 && (
+            <List>
+              <Typography variant="subtitle1" sx={{mt: 2}}>
+                Tilgængelige tidsserier:
+              </Typography>
+              {unit_history?.map((sensor) => (
+                <Box
+                  key={sensor.uuid}
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={checkedSensors.some((s) => s.uuid === sensor.uuid)}
+                        onChange={(e) => {
+                          if (e.target.checked) {
+                            setCheckedSensors((prev) => [...prev, sensor]);
+                          } else {
+                            setCheckedSensors((prev) => prev.filter((s) => s.uuid !== sensor.uuid));
+                          }
+                        }}
+                      />
+                    }
+                    label={
+                      <Typography>
+                        {sensor.signal_id} - {sensor.sensor_id} (
+                        {activeTimeseries?.find((ts) => ts.ts_id === sensor.ts_id)?.ts_name})
+                      </Typography>
+                    }
+                  />
+                </Box>
+              ))}
+            </List>
+          )}
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button bttype="tertiary" onClick={handleClose}>
+          Annuller
+        </Button>
+        <Button
+          bttype="primary"
+          disabled={checkedSensors.length === 0 || Object.keys(errors).length > 0}
+          onClick={handleSubmit(async (data) => {
+            await takeHomeMutation({
+              enddate: data.enddate,
+              change_reason: data.change_reason,
+              action: data.action,
+              comment: data.comment,
+              units: checkedSensors.map((sensor) => ({
+                ts_id: sensor.ts_id,
+                gid: sensor.gid,
+                startdate: dayjs(sensor.startdato),
+              })),
+            });
+          })}
+        >
+          Hjemtag udstyr
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
